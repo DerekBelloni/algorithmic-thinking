b2cfd0cf92f327632c97a43da5486c782b449cce
Derek Belloni <derekbelloni10@gmail.com>
2025-03-25 21:45:10 -0600

    started modifying the problem for identical snowflakes to use a linked list vs a two dimensional array

--- chapter_one/main.c
+++ chapter_one/main.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define ARMS 6
+#define SIZE 100000
 
-
+typedef struct snowflake_node {
+    int snowflake[6];
+    struct snowflake_node *next;
+} snowflake_node;
 
 int checkClockwise(int snowflake1[6], int snowflake2[6], int start) {
-    int offset, startingIndex;
+    int offset, snow2Idx;
     for (offset = 0; offset < ARMS; offset++) {
-        startingIndex = start + offset;
-        if (startingIndex >= 6) startingIndex = startingIndex - 6;
-        if (snowflake1[offset] != snowflake2[startingIndex]) {
+        snow2Idx = start + offset;
+        if (snow2Idx >= 6) snow2Idx = snow2Idx - 6;
+        if (snowflake1[offset] != snowflake2[snow2Idx]) {
             return 0;
         }
     }
@@ -19,11 +23,11 @@ int checkClockwise(int snowflake1[6], int snowflake2[6], int start) {
 }
 
 int checkCounterClockwise(int snowflake1[6], int snowflake2[6], int start) {
-    int offset, startingIndex;
+    int offset, snow2Idx;
     for (offset = 0; offset < ARMS; offset++) {
-        startingIndex = start - offset;
-        if (startingIndex <= -1) startingIndex = startingIndex + 6;
-        if (snowflake1[offset] != snowflake2[startingIndex]) {
+        snow2Idx = start - offset;
+        if (snow2Idx <= -1) snow2Idx = snow2Idx + 6;
+        if (snowflake1[offset] != snowflake2[snow2Idx]) {
             return 0;
         }
     }
@@ -45,25 +49,43 @@ int areIdentical(int snowflake1[6], int snowflake2[6]) {
     return 0;
 }
 
-void identifyIdentical(int snowflakes[2][6]) {
+void identifyIdentical(int snowflakes[][6], int n) {
     int i, j; 
-    if (areIdentical(snowflakes[0], snowflakes[1])) {
-        printf("Two identical snowflakes found.\n");
-        return;
+
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (areIdentical(snowflakes[i], snowflakes[j])) {
+                printf("Two identical snowflakes found.\n");
+                return;
+            }
+        }
     }
+    
     printf("No two snowflakes are alike.\n");
 }
 
+int code(int snowflake[]) {
+    return (snowflake[0] + snowflake[1] + snowflake[2] + snowflake[3] + snowflake[4] + snowflake[5]) % SIZE;
+}
+
 int main() {
-//   int a[5] = {1, 2, 3, 1, 5};
-//    identifyIdentical(a, 5);
+    static snowflake_node snowflakes[SIZE] = {NULL};
+    snowflake_node *snow;
+    int n, i, j, snowflake_code;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        snow = malloc(sizeof(snowflake_node));
+        if (snow == NULL) {
+            printf("malloc err\n");
+            return -1;
+        }
+        for (j = 0; j < 6; j++) {
+            scanf("%d", &snow->snowflake[j]);
+        }
+        snowflake_code = code(snow->snowflake);
 
-    int snowflakes [2][6] = {
-        {1, 2, 3, 4, 5, 6},
-        {3, 4, 5, 6, 1, 2}
-    };
+    }
 
-    identifyIdentical(snowflakes);
-
+//    identifyIdentical(snowFlakes, n);
     return 0;
 }
